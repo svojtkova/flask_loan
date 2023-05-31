58ce05da3a80f7c2690e1ee353c39d6c64fe3280
svojtkova <svetlana.vojtkova@gmail.com>
2023-05-31 22:03:27 +0200

    api loan call, requirements

--- main.py
+++ main.py
@@ -1,12 +1,49 @@
-from flask import Flask, jsonify
+from flask import Flask, request
+from flask_restx import Api, Resource
 import os
+import json
+import pandas as pd
+import joblib
+import sklearn
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
+api = Api(app, version='1.0', title='Your API', description='API documentation with Swagger')
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+ns = api.namespace('api', description='Loan API calls')
+
+@ns.route('/loan')
+class LoadDefault(Resource):
+    def post(self):
+        data = request.get_json()
+        print(data)  # Retrieve the JSON data from the request
+        df2 = pd.json_normalize(data)
+        
+        #print(df)
+        df2['term'] = df2['term'].astype(str).astype(int)
+        df2['grade'] = df2['grade'].astype(str).astype(int)
+        df2['subgrade'] = df2['subgrade'].astype(str).astype(int)
+        df2['verification_status'] = df2['verification_status'].astype(str).astype(int)
+
+        #loaded_model = joblib.load('random_forest_model.joblib')
+
+        #probabilities = loaded_model.predict_proba(df2)
+
+        # Print the predicted probabilities for each sample
+        #for i, probs in enumerate(probabilities):
+        #    class_0_prob = probs[0]
+        #    class_1_prob = probs[1]
+        #    string = "Probability to be fully paid: {0:.2f}, Probability to default: {1:.2f}".format(class_0_prob,class_1_prob)
+
+        # Perform further processing with the DataFrame if needed
+
+        # Convert DataFrame to JSON
+        #response_data = df.to_json(orient='records')
+        string = "Fine"
+        # Return JSON response
+        return {"message":string}#response_data
 
 
 if __name__ == '__main__':
