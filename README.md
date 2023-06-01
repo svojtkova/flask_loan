0a93ca52727c21d9eaf7faec08b55af28514fbd7
Svetlana <83221957+svojtkova@users.noreply.github.com>
2023-06-01 22:19:25 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,23 +1,3 @@
----
-title: Flask
-description: A popular minimal server framework for Python
-tags:
-  - python
-  - flask
----
+# Default Prediction Flask API
 
-# Python Flask Example
-
-This is a [Flask](https://flask.palletsprojects.com/en/1.1.x/) app that serves a simple JSON response.
-
-[![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/new/template/zUcpux)
-
-## ✨ Features
-
-- Python
-- Flask
-
-## 💁‍♀️ How to use
-
-- Install Python requirements `pip install -r requirements.txt`
-- Start the server for development `python3 main.py`
+This is a Flask API for a default prediction application. It provides an endpoint to make predictions using a trained model. The application includes the code, model file, Jupyter notebook used for model preparation, and project documentation.
--- main.py
+++ main.py
@@ -9,41 +9,41 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app)
-api = Api(app, version='1.0', title='Your API', description='API documentation with Swagger')
+api = Api(app, version='1.0', title='Your API',
+          description='API documentation with Swagger')
 
 
 ns = api.namespace('api', description='Loan API calls')
 
+loaded_model = joblib.load('random_forest_model.joblib')
+
+
 @ns.route('/loan')
 class LoadDefault(Resource):
     def post(self):
-        data = request.get_json()
-        print(data)  # Retrieve the JSON data from the request
-        df2 = pd.json_normalize(data)
-        
-        #print(df)
-        df2['term'] = df2['term'].astype(str).astype(int)
-        df2['grade'] = df2['grade'].astype(str).astype(int)
-        df2['subgrade'] = df2['subgrade'].astype(str).astype(int)
-        df2['verification_status'] = df2['verification_status'].astype(str).astype(int)
+        try:
+            string = ''
+            data = request.get_json()
+            df2 = pd.json_normalize(data)
 
-        #loaded_model = joblib.load('random_forest_model.joblib')
+            df2['term'] = df2['term'].astype(str).astype(int)
+            df2['grade'] = df2['grade'].astype(str).astype(int)
+            df2['subgrade'] = df2['subgrade'].astype(str).astype(int)
+            df2['verification_status'] = df2['verification_status'].astype(
+                str).astype(int)
 
-        #probabilities = loaded_model.predict_proba(df2)
+            probabilities = loaded_model.predict_proba(df2)
 
-        # Print the predicted probabilities for each sample
-        #for i, probs in enumerate(probabilities):
-        #    class_0_prob = probs[0]
-        #    class_1_prob = probs[1]
-        #    string = "Probability to be fully paid: {0:.2f}, Probability to default: {1:.2f}".format(class_0_prob,class_1_prob)
+            for i, probs in enumerate(probabilities):
+                class_0_prob = probs[0]
+                class_1_prob = probs[1]
+                string = 'Probability to be fully paid: {0:.0f}%. Probability to default: {1:.0f}%'.format(
+                    class_0_prob*100, class_1_prob*100)
 
-        # Perform further processing with the DataFrame if needed
+            return { 'message': string }
 
-        # Convert DataFrame to JSON
-        #response_data = df.to_json(orient='records')
-        string = "Fine"
-        # Return JSON response
-        return {"message":string}#response_data
+        except Exception as e:
+            return {'error': str(e)}, 500
 
 
 if __name__ == '__main__':
