736f07211eeee6daff7a1fe688e260ed0805ba6a
svojtkova <svetlana.vojtkova@gmail.com>
2023-06-01 23:11:22 +0200

    joblib

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ api = Api(app, version='1.0', title='Your API',
 
 ns = api.namespace('api', description='Loan API calls')
 
-loaded_model = joblib.load('random_forest_model.joblib')
+
 
 
 @ns.route('/loan')
@@ -31,6 +31,8 @@ class LoadDefault(Resource):
             df2['subgrade'] = df2['subgrade'].astype(str).astype(int)
             df2['verification_status'] = df2['verification_status'].astype(
                 str).astype(int)
+            
+            loaded_model = joblib.load('random_forest_model.joblib')
 
             probabilities = loaded_model.predict_proba(df2)
 
