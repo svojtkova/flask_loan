0d8cd3ec0eeebe90b595061dbab39bb594556b2f
svojtkova <svetlana.vojtkova@gmail.com>
2023-06-02 12:12:15 +0200

    model zip

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ ns = api.namespace('api', description='Loan API calls')
 #loaded_model = pickle.load(open(fname, 'rb'))
 
 
-with zipfile.ZipFile('./random_forest_model.zip', 'r') as zip_file:
+with zipfile.ZipFile('random_forest_model.zip', 'r') as zip_file:
     zip_file.extractall()
 
-fname = './random_forest_model.pkl'
+fname = 'random_forest_model.pkl'
 loaded_model =  pickle.load(open(fname,'rb'))
 
 
